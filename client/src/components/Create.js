1e7b0465d659b22d170288579e8b93a98d05832c
Carlos G <carlosjr3000@hotmail.com>
2018-06-01 16:49:32 -0500

    created new user form

--- client/src/components/Create.js
+++ client/src/components/Create.js
@@ -1,15 +1,48 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
-import {Link} from 'react-router-dom';
-import './components.css'
+import {BrowserRouter as Router, Link} from 'react-router-dom';
+import Route from 'react-router/Route'
+import './create.css'
 
 
 class Create extends Component {
     render() {
         return ( 
             <div className='main-div'>
-                <h1> This is the <u>Create</u> component </h1> 
+               <form action="">
+                <div className="container">
+                        <h1>Register</h1>
+                        <p>Please fill in this form to create an account.</p>
+                        <hr />
+                        <div>
+                            <label htmlFor="Name" className="lb1"><b>Name</b></label>
+                            <input type="text" placeholder="Enter Name" name="Name" required />
+                        </div>
+                      <div>
+                        <label htmlFor="email" className="lb1"><b>Email</b></label>
+                        <input type="text" placeholder="Enter Email" name="email" required />
+                      </div>
+
+                      <div>
+                        < label htmlFor = "psw" className = "lb2"> <b> Password </b></label >
+                        <input type="password" placeholder="Enter Password" name="psw" required />
+                      </div>
+
+                      <div>
+                        <label htmlFor="psw-repeat" className="lb3"><b>Repeat Password</b></label>
+                        <input type="password" placeholder="Repeat Password" name="psw-repeat" required/>
+                        <hr />
+                     </div>                        
+                        <button type="submit" className="registerbtn">Register</button>
+                     <div>
+                         <p>Already have an account? <Link to="/">Sign in</Link></p>
+                     </div>
+
+                </div>
+                    
+                  
+                </form>
             </div>
         );
     }
