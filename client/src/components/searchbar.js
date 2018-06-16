a2dea540a4dce0d3fa22532f9461319478097ec5
Mirandamm228 <codingwith.miranda@gmail.com>
2018-06-16 14:48:48 -0700

    set up a lot of the search logic, need to make requests into the db

--- client/src/components/searchbar.js
+++ client/src/components/searchbar.js
@@ -2,40 +2,80 @@
 //still need to research how search bar logic works
 
 import React from 'react';
-import { Form, FormControl, FormGroup, Button } from 'react-bootstrap';
+import { Form, FormControl, FormGroup } from 'react-bootstrap';
+import axios from 'axios';
+
 
 export default class Searchbar extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            search: ''
-        }
+            titles: '',
+            authors: ''
+        };
     }
 
-    search(e){
-        this.setState({search: e.target.value})
-        //use regex to filter name through titles?
-        //http://frontendcollisionblog.com/javascript/jekyll/tutorial/2015/03/26/getting-started-with-a-search-engine-for-your-site-no-server-required.html
-        //http://zofiakreasi.com/pure-html5-and-javascript-search-engine/
-        //https://www.youtube.com/watch?v=7-ToBxPx2dA
-
-        //or even better! Use .findOne({}) for mongoDB and render the results and alphabetize
-        //.findOne({where "title" : "`${input.title}`" AND where "author" : "`${input.author}`"})
-        //set the results to an array, and .sort() then .map() them rendering each one. Boo yeah! 
 
 
+
+
+
+    updateTitles(e){
+        axios.get(`https://localhost:5000/api/books/`)
+            .then(res => {
+                console.log(res);
+            });
+        this.setState({titles: e.target.value});
+
+
+    }
+
+    updateAuthors(e){
+        this.setState({authors: e.target.value});
     }
 
     render(){
+        let filteredTitles = this.props.books.filter((book) => {
+            return book.title.indexOf(this.state.titles) !== -1;
+        });
+        const listTitles = filteredTitles.map((book, index) => (
+            <li key={index}>
+              {book.title}: {book.author}
+            </li>
+          ));
+        let filteredAuthors = this.props.books.filter((book) => {
+            return book.author.indexOf(this.state.authors) !== -1;
+        });
+        const listAuthors = filteredAuthors.map((book, index) => (
+            <li key={index}>
+              {book.title}: {book.author}
+            </li>
+        ));
         return(
-            <Form inline>
+            <div>
+                <Form inline>
                 <FormGroup controlId="form">
-                    <FormControl type="text" placeholder="Search for books" id="search"/>
-                </FormGroup>
+                    <FormControl type="text" 
+                        placeholder="Search for titles"  
+                        value={this.state.titles}
+                        onChange={this.updateSearch.bind(this)}/> <br/>
+                    <FormControl type="text"
+                        placeholder="Search for authors"
+                        value={this.state.authors}
+                        onChange={this.updateAuthors.bind(this)}/>
+                </FormGroup> 
+                </Form>
+                <div>
+                    <ul>
+                        {listTitles}
+                    </ul>
+                    <ul>
+                        {listAuthors}
+                    </ul>
+                </div>
+            </div>
+            
 
-                <Button type="submit" onClick={this.search.bind(this)}>Find 'em!</Button> 
-        
-            </Form>
         );
     }
 }
