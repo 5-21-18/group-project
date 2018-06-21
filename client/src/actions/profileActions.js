ba8ec0ab4d54921692d974d8500d94cf15d12590
Carlos G <carlosjr3000@hotmail.com>
2018-06-20 19:11:49 -0500

    added logic to view profiles

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -2,11 +2,25 @@ import axios from "axios";
 
 import {
   GET_PROFILE,
+  GET_PROFILES,
   PROFILE_LOADING,
   CLEAR_CURRENT_PROFILE,
   GET_ERRORS,
   SET_CURRENT_USER
 } from "./types";
+
+// Create Profile
+export const createProfile = (profileData, history) => dispatch => {
+  axios
+    .post("/api/profile", profileData)
+    .then(res => history.push("/dashboard"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
 
 // Get current profile
 export const getCurrentProfile = () => dispatch => {
@@ -27,15 +41,40 @@ export const getCurrentProfile = () => dispatch => {
     );
 };
 
-// Create Profile
-export const createProfile = (profileData, history) => dispatch => {
+// Get profile by handle
+export const getProfileByHandle = handle => dispatch => {
+  dispatch(setProfileLoading());
   axios
-    .post("/api/profile", profileData)
-    .then(res => history.push("/dashboard"))
+    .get(`/api/profile/handle/${handle}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
     .catch(err =>
       dispatch({
-        type: GET_ERRORS,
-        payload: err.response.data
+        type: GET_PROFILE,
+        payload: null
+      })
+    );
+};
+
+// Get all profiles
+export const getProfiles = () => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .get("/api/profile/all")
+    .then(res =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: null
       })
     );
 };
