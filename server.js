b7ce7c0409fc4fde27266da929c3694e3f4384af
Carlos G <carlosjr3000@hotmail.com>
2018-06-22 10:01:02 -0500

    add public folder to server side

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const favicon = require("serve-favicon");
+const path = require("path");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
